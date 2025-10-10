01478469658b1f3b3ff365de6f32d1184a31da20
xplozion15 <89824888+xplozion15@users.noreply.github.com>
2025-10-10 13:00:39 +0530

    chore: install ejs

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+// app.js
+const path = require("node:path");
+
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+
+// app.js
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
